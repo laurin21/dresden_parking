bd10761236207a57207c8c120f9a2deaf579df7b
laurin21 <74432244+laurin21@users.noreply.github.com>
2025-07-28 18:59:34 +0200

    Update dresden_parking.py

--- dresden_parking.py
+++ dresden_parking.py
@@ -199,6 +199,26 @@ view_state = pdk.ViewState(latitude=51.0504, longitude=13.7373, zoom=13)
 # Karte anzeigen
 st.pydeck_chart(pdk.Deck(layers=[scatter_layer], initial_view_state=view_state, tooltip=tooltip))
 
+# --- Legende für die Karte ---
+st.markdown("### Legend")
+col1, col2 = st.columns([1, 5])
+with col1:
+    st.markdown("<div style='width:20px;height:20px;background-color:rgb(0,255,0)'></div>", unsafe_allow_html=True)
+with col2:
+    st.markdown("Low predicted occupation")
+
+col1, col2 = st.columns([1, 5])
+with col1:
+    st.markdown("<div style='width:20px;height:20px;background-color:rgb(255,255,0)'></div>", unsafe_allow_html=True)
+with col2:
+    st.markdown("Medium predicted occupation")
+
+col1, col2 = st.columns([1, 5])
+with col1:
+    st.markdown("<div style='width:20px;height:20px;background-color:rgb(255,0,0)'></div>", unsafe_allow_html=True)
+with col2:
+    st.markdown("High predicted occupation")
+
 st.markdown("---")
 
 show_debug = st.toggle("Debugging Mode")

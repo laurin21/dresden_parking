feb1ffc6ae447ec677598d396a630efbc2a0cbb8
laurin21 <74432244+laurin21@users.noreply.github.com>
2025-07-28 19:00:15 +0200

    Update dresden_parking.py

--- dresden_parking.py
+++ dresden_parking.py
@@ -200,7 +200,7 @@ view_state = pdk.ViewState(latitude=51.0504, longitude=13.7373, zoom=13)
 st.pydeck_chart(pdk.Deck(layers=[scatter_layer], initial_view_state=view_state, tooltip=tooltip))
 
 # --- Legende für die Karte ---
-st.markdown("### Legend")
+st.markdown("#### Legend")
 col1, col2 = st.columns([1, 5])
 with col1:
     st.markdown("<div style='width:20px;height:20px;background-color:rgb(0,255,0)'></div>", unsafe_allow_html=True)
